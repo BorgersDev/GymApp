191cd5033caecd2d3c63655dea2c3e031ca4ea89
Arthur Borges <borgesarthur.dev@gmail.com>
2024-08-09 14:26:28 -0300

    HistoryCard component

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,21 @@
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
+
+
+export const HistoryCard = () => {
+    return (
+        <HStack w="$full" px="$5" py="$4" mb="$3" bg="$gray600" rounded="$md" alignItems="center" justifyContent="space-between">
+            <VStack mr="$5" >
+                <Heading color="$white" fontSize="$md" fontFamily="$heading" textTransform="capitalize" >
+                    Costas
+                </Heading>
+
+                <Text color="$gray100" fontSize="$lg" textTransform="capitalize" >
+                    Puxada Frontal
+                </Text>
+            </VStack>
+            <Text color="$gray300" fontSize="$md" >
+                08:56
+            </Text>
+        </HStack>
+    )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,14 @@
-import { ScreenHeader } from "@components/ScreenHeader"
 import { VStack } from "@gluestack-ui/themed"
+
+import { HistoryCard } from "@components/HistoryCard"
+import { ScreenHeader } from "@components/ScreenHeader"
 
 
 export const History = () => {
     return (
         <VStack flex={1}>
             <ScreenHeader title="Histórico" />
+            <HistoryCard />
         </VStack>
     )
 }

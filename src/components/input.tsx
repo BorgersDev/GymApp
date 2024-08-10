a165c0529fae3e12dc29da693c2e8afa7a521709
Arthur Borges <borgesarthur.dev@gmail.com>
2024-08-09 21:19:06 -0300

    Profile screen finalized

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,21 +1,30 @@
 import { ComponentProps } from "react"
 import { Input as GSInput, InputField } from "@gluestack-ui/themed"
 
-type Props = ComponentProps<typeof InputField>
+type Props = ComponentProps<typeof InputField> & {
+    isReadOnly?: boolean;
+}
 
-export const Input = ({...rest}: Props) => {
+export const Input = ({isReadOnly,...rest}: Props) => {
     return (
-        <GSInput 
-         bg="$gray700" 
-         h="$12" px="$4" 
+        <GSInput  
+         h="$12"  
          borderWidth="$0" 
          borderRadius="$lg" 
          $focus={{
             borderWidth: 1,
             borderColor: "$green500"
          }}
+         isReadOnly={isReadOnly}
+         opacity={isReadOnly ? 0.4 : 1}
         >
-            <InputField color="$white" fontFamily="$body" placeholderTextColor="$gray300" {...rest} />
+            <InputField 
+                bg="$gray700"
+                px="$4"
+                color="$white" 
+                fontFamily="$body" 
+                placeholderTextColor="$gray300" 
+                {...rest} />
         </GSInput>
     )
 }
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,59 @@
-import { Center, Text } from "@gluestack-ui/themed"
+import { ScrollView, TouchableOpacity } from "react-native"
+
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed"
+
+import { ScreenHeader } from "@components/ScreenHeader"
+import { UserPic } from "@components/UserPic"
+import { Input } from "@components/input"
+import { Button } from "@components/Button"
 
 
 export const Profile = () => {
     return (
-        <Center flex={1}>
-            <Text>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil" />
+
+            <ScrollView 
+                contentContainerStyle={{flexGrow: 1 ,paddingBottom: 36}} 
+                showsVerticalScrollIndicator={false} 
+                automaticallyAdjustKeyboardInsets 
+                bounces={false}
+            >
+                <Center mt="$4" px="$10" >
+                    <UserPic 
+                        source={{uri: "https://github.com/BorgersDev.png"}} 
+                        alt="Foto do usuário" 
+                        size="xl"
+                    />
+                    <TouchableOpacity>
+                        <Text color="$green500" fontFamily="$heading" fontSize="$md" mt="$2" mb="$8" >
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Center w="$full" gap="$3" >
+                        <Input placeholder="Nome" bg="$gray600" />
+                        <Input value="arthur@email.com" bg="$gray600" isReadOnly/>
+                    </Center>
+                    <Heading 
+                        alignSelf="flex-start"
+                        fontFamily="$heading"
+                        color="$gray200"
+                        fontSize="$md"
+                        mt="$9"
+                        mb="$2"
+                    >
+                            Alterar senha
+                    </Heading>
+                    <Center w="$full" gap="$3" >
+                        <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+                        <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+                        <Input placeholder="Confirmar nova senha" bg="$gray600" secureTextEntry />
+
+                        <Button title="Atualizar senha" />
+                    </Center>
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }

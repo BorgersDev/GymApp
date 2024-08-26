755159ec7bef25f16dbf98d733f6f6373669112b
Arthur Borges <borgesarthur.dev@gmail.com>
2024-08-26 11:48:53 -0300

    Requisitions Queue implemented

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,27 @@
 import { getAuthTokenStorage } from "@storage/authTokenStorage";
 import { AppError } from "@utils/AppError";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 type SignOut = () => void;
 
+type PromiseType = {
+    onSuccess: (token: string) => void;
+    onFailure: (error: AxiosError) => void;
+}
+
 type ApiInstanceProps = AxiosInstance & {
-    registerInterceptTokenManager: (SignOut: SignOut) => () => void;
+    registerInterceptTokenManager: (signOut: SignOut) => () => void;
 }
 
 const api = axios.create({
     baseURL: "http://10.0.0.113:3333"
 }) as ApiInstanceProps
 
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
+
 api.registerInterceptTokenManager = signOut =>  {
-    const InterceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
+    const interceptTokenManager = api.interceptors.response.use(response => response, async (requestError) => {
         if(requestError?.response?.status === 401) {
             if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
                 const { refresh_token } = await getAuthTokenStorage();
@@ -23,6 +31,24 @@ api.registerInterceptTokenManager = signOut =>  {
 
                     return Promise.reject(requestError);
                 }
+
+                const originalRequestConfig = requestError.config;
+
+                if(isRefreshing) {
+                    return new Promise((resolve, reject) => {
+                        failedQueue.push({
+                            onSuccess: (token: string) => {
+                                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}` }
+                                resolve(api(originalRequestConfig));
+                            },
+                            onFailure: (error: AxiosError) => {
+                                reject(error);
+                            },
+                        })
+                    });
+                }
+
+                isRefreshing = true;
             }
             signOut();
         }
@@ -38,8 +64,9 @@ api.registerInterceptTokenManager = signOut =>  {
     });
 
     return () => {
-        api.interceptors.response.eject(InterceptTokenManager)
+        api.interceptors.response.eject(interceptTokenManager)
     }
+
 }
 
 export {api}
